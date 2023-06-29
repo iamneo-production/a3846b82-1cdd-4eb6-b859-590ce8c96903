5714ceb57f3e3110c18f491a3f02316078764f76
PAVITHRA-8 <pavithrasundaram2001@gmail.com>
2023-06-29 09:18:59 +0000

    sidenav page added

--- TaskManagement/src/app/app.module.ts
+++ TaskManagement/src/app/app.module.ts
@@ -1,20 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { TaskDetailsComponent } from './task-details/task-details.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserDetailsComponent,
-    TaskDetailsComponent
+    TaskDetailsComponent,
+    SidenavComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatMenuModule,
+    MatButtonModule
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
