b4371ec55652c288316fa9b0698abf65f8fb571a
PAVITHRA-8 <pavithrasundaram2001@gmail.com>
2023-06-29 08:42:28 +0000

    added routing paths

--- TaskManagement/src/app/app-routing.module.ts
+++ TaskManagement/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
 
-const routes: Routes = [{path:"userdetails",component:UserDetailsComponent}];
+const routes: Routes = 
+[{path:"**",component:UserDetailsComponent},
+{path:"userdetails",component:UserDetailsComponent},
+{path:"taskdetails",component:TaskDetailsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
