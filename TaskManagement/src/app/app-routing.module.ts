e589dbc99b2320e2a0f2fca164c052bfd6357601
SuvethaMurugesan <25suvetha@gmail.com>
2023-06-30 15:47:51 +0000

    modified

--- TaskManagement/src/app/app-routing.module.ts
+++ TaskManagement/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { TaskDetailsComponent } from './task-details/task-details.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = 
 [{path:"**",component:UserDetailsComponent},
 {path:"userdetails",component:UserDetailsComponent},
-{path:"taskdetails",component:TaskDetailsComponent}];
+{path:"taskdetails",component:TaskDetailsComponent},
+{path:"signup",component:SignupComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TaskManagement/src/app/app.module.ts
+++ TaskManagement/src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VerificationComponent } from './verification/verification.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { TaskDetailsComponent } from './task-details/task-details.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VerificationComponent,
     UserDetailsComponent,
-    TaskDetailsComponent
+    TaskDetailsComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
