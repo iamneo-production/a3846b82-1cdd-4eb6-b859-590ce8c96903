4a25f513eafd25dce91046b22718528d3d25e117
chennasandeepreddy <chennasandeepreddy@gmail.com>
2023-07-02 05:08:44 +0000

    added code for test cases

---
+++ springapp/src/main/java/com/example/springapp/repository/TaskRepository.java
@@ -0,0 +1,13 @@
+package com.example.springapp.repository;
+
+import com.example.springapp.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+    List<Task> findByUser_Username(String username);
+    Task findByUser_UsernameAndId(String username, int id);
+}
