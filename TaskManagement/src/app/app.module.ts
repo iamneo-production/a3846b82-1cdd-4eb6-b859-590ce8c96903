6f60c10e65a6752d0e1d632bb5ef8d51b0540483
HemaPriyaP21 <hema.pshg@gmail.com>
2023-07-02 06:19:27 +0000

    added

--- TaskManagement/src/app/app.module.ts
+++ TaskManagement/src/app/app.module.ts
@@ -18,6 +18,13 @@ import { LoginComponent } from './login/login.component';
 import { AssignTaskComponent } from './assign-task/assign-task.component';
 import { CreateTaskComponent } from './create-task/create-task.component';
 import { ReportingComponent } from './reporting/reporting.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { CUSTOM_ELEMENTS_SCHEMA }      from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -37,6 +44,7 @@ import { ReportingComponent } from './reporting/reporting.component';
     AssignTaskComponent,
     CreateTaskComponent,
     ReportingComponent,
+    CalendarComponent,
   ],
   imports: [
     BrowserModule,
@@ -44,7 +52,13 @@ import { ReportingComponent } from './reporting/reporting.component';
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModalModule,
+    CommonModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory}),
   ],
   providers: [],
   bootstrap: [AppComponent]
