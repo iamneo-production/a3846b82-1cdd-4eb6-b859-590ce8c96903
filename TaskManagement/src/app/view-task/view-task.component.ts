865a72b6145ad1d366829806f986e4bbd9a320b4
PAVITHRA-8 <pavithrasundaram2001@gmail.com>
2023-07-03 06:07:12 +0000

    view task created

--- TaskManagement/src/app/view-task/view-task.component.ts
+++ TaskManagement/src/app/view-task/view-task.component.ts
@@ -1,15 +1,5 @@
 import { Component } from '@angular/core';
 
-export class {
-  constructor(
-    public id: number,
-    public description :string,
-    public done:boolean,
-    public targetDate:Date,
-  
-  ){
-    
-  }
 
 @Component({
   selector: 'app-view-task',
