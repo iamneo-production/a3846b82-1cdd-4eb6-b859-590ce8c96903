48f9d4a69cb0b06966d8cf81ea6906b00f9c7754
PAVITHRA-8 <pavithrasundaram2001@gmail.com>
2023-07-11 15:03:50 +0000

    backend added

--- TaskManagement/src/app/service/todo/todo-data.service.ts
+++ TaskManagement/src/app/service/todo/todo-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Todo } from 'src/app/view-task/view-task.component';
-import { TODO_JPA_API_URL } from 'src/app/app.constants';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,26 +13,11 @@ export class TodoDataService {
 
   retrieveAllTodos(){
 
-    return this.http.get<Todo[]>(`${TODO_JPA_API_URL}/view-task`);
+    return this.http.get<Todo[]>(`https://8080-dfbdbabdfcfdedeaeaadbdbabf.project.examly.io/view-task`);
   
    }
 
-   
-  retriveTodo(taskName: any , id: any){
-    return this.http.get<Todo>(`${TODO_JPA_API_URL}/${taskName}/view-task/${id}`);
-  }
 
-  deleteTodo(taskName: any , id: any){
-    return this.http.delete(`${TODO_JPA_API_URL}/${taskName}/view-task/${id}`);
-  }
 
-  updateTodo(taskName: any , id: any,todo: any){
-    return this.http.put(
-      `${TODO_JPA_API_URL}/${taskName}/view-task/${id}`,todo) ; 
-    }
-
-    createTodo(taskName: any , todo:any){
-      return this.http.post(`${TODO_JPA_API_URL}/${taskName}/view-task` , todo);
-    }
-
+  
 }
