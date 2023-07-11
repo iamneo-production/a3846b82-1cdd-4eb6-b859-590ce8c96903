e89f02f554872a147905aa306b0bf87bd1c1ec83
PAVITHRA-8 <pavithrasundaram2001@gmail.com>
2023-07-11 12:44:41 +0000

    changes updated

---
+++ taskmanagement/src/main/java/com/project/taskmanagement/controller/TodoController.java
@@ -0,0 +1,85 @@
+package com.project.taskmanagement.controller;
+
+import java.net.URI;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
+
+@CrossOrigin("https://8081-dfbdbabdfcfdedeaeaadbdbabf.project.examly.io")
+@RestController  
+public class TodoController {
+	
+   @Autowired 
+   private TodoRepository todoRepository;
+   
+   @GetMapping("/jpa/view-task")
+   public List<Todo> retrieveAllTodos(){
+	   return todoRepository.findAll();
+	   }
+   
+	@GetMapping("/jpa/{taskName}/view-task/{id}")
+	public Todo getTodoById (@PathVariable String taskName,@PathVariable Long id){
+		return todoRepository.findById(id).get();
+	
+	}
+	
+	
+	@GetMapping("/jpa/{taskName}/view-task")
+	public Todo getTodoByTaskName (@PathVariable String taskName){
+		return todoRepository.findByTaskName(taskName).get(0);
+		
+	}
+	
+	@DeleteMapping("/jpa/{taskName}/view-task/{id}")
+	public ResponseEntity<Void> deleteTodo(
+			@PathVariable String taskName, @PathVariable Long id) 
+	    {   	
+		todoRepository.deleteById(id);
+		
+		return ResponseEntity.noContent().build();  
+	}
+	
+	
+	@PutMapping ("/jpa/{taskName}/view-task/{id}")
+	public ResponseEntity<Todo> updateTodo(
+			@PathVariable String taskName,
+			@PathVariable Long id, @RequestBody Todo todo    
+			 ){ 
+		
+		Todo todoUpdated = todoRepository.save(todo); 
+		
+	    return new ResponseEntity<Todo>(todo,HttpStatus.OK); 
+	    
+	    
+	}
+	
+	
+	@PostMapping("/jpa/{taskName}/view-task")
+	public ResponseEntity<Void> createTodo(
+			@PathVariable String taskName,
+			 @RequestBody Todo todo
+			 ){
+		Todo createdTodo  = todoRepository.save(todo);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+		.path("/{id}").buildAndExpand(createdTodo.getId()).toUri(); 
+		
+	    return ResponseEntity.created(uri).build();
+	
+	
+	}
+}
+
+
---
+++ taskmanagement/src/main/java/com/project/taskmanagement/service/TodoService.java
@@ -0,0 +1,68 @@
+
+package com.project.taskmanagement.service;
+
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoHardcodedService {
+	
+	
+	
+	private static List<Todo> todos = new ArrayList<Todo>();
+	
+    private static long idCounter=0;
+	
+	static {
+		
+		todos.add(new Todo(++idCounter,"ui/ux","learn to design",new Date(),false, true));
+		todos.add(new Todo(++idCounter,"html","learn to create web",new Date(),true, true));
+		todos.add(new Todo(++idCounter,"css","learn to style web",new Date(),false, false));
+		todos.add(new Todo(++idCounter,"sql/db","learn to create db",new Date(),true, false));
+	}
+	
+	public List<Todo> findAll(){
+		return todos;
+	}
+	
+    public Todo save(Todo todo) {
+    	if(todo.getId()==-1 || todo.getId()==0)  
+    	{   
+    		todo.setId(++idCounter);    
+    		todos.add(todo);         
+    		 
+    		
+    		
+    	}else {
+    		deleteById(todo.getId());  
+    		todos.add(todo);           
+    		
+    	}
+		return todo;
+    	
+    }
+    
+    //created delete method will return the deleted todo back
+	public Todo deleteById(long id) {  //long id -- the id which we want to pass in 'can delete using this specific id'
+		Todo todo =findById(id);
+		if(todo==null) return null;  //checking at this point 
+		if(todos.remove(todo)) {     //able to delete the todo using remove method it returns true or false
+		return todo;                 //if the todo removed successfully it will return the todo back
+	} 
+	return null;	                 //or it will return null
+	}
+	
+//created a method for findById will return the todo back
+	
+	Todo findById(long id) {
+		for(Todo todo:todos) {        //iterated to all the todo's
+			if(todo.getId()==id) {
+				return todo;
+			}
+		}
+		return null;
+	}
+}
