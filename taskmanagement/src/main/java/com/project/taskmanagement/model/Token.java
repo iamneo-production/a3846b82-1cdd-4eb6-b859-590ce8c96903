7306da37b772f0a9ff5b15dc2341b325d4c5a44d
chennasandeepreddy <chennasandeepreddy@gmail.com>
2023-07-14 17:36:21 +0000

    resolving

---
+++ taskmanagement/src/main/java/com/project/taskmanagement/model/Token.java
@@ -0,0 +1,37 @@
+package com.project.taskmanagement.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="token")
+public class Token {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	private String token;
+	private boolean expired;
+	private boolean revoked;
+	
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	private User user;
+	
+	
+}
