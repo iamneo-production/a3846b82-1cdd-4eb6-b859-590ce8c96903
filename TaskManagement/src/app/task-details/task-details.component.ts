4382ac632fcb6af1c769ba7f30816d80565cd3bf
chennasandeepreddy <chennasandeepreddy@gmail.com>
2023-07-22 14:35:47 +0000

    updated task details

--- TaskManagement/src/app/task-details/task-details.component.ts
+++ TaskManagement/src/app/task-details/task-details.component.ts
@@ -1,49 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskserviceService } from '/home/coder/project/workspace/TaskManagement/src/app/service/data/taskservice.service'
 
-export class Task{
-  constructor(
-    public taskdescription:string,
-	  public taskname:string,
-	  public taskduedate:Date,
-	  public id:number,
-	  public username:string,
-	  public taskstatus:boolean
-  ){
-  
-  }
+export interface Task {
+  id: number;
+  username: string;
+  taskname: string;
+  taskdescription: string;
+  taskduedate: Date;
+  taskstatus: boolean;
 }
+
 @Component({
   selector: 'app-task-details',
   templateUrl: './task-details.component.html',
-  styleUrls: ['./task-details.component.css']
+  styleUrls: ['./task-details.component.css'],
 })
-export class TaskDetailsComponent implements OnInit{
-  message:any;
-  constructor(public taskservice :TaskserviceService){
+export class TaskDetailsComponent implements OnInit {
+  message: any;
+  tasks: Task[] | undefined;
 
-  }
-  
+  constructor(public taskservice: TaskserviceService) {}
+
   ngOnInit(): void {
-    this.refreshTasks()
+    this.refreshTasks();
   }
-  refreshTasks(){
-    this.taskservice.retrieveTasks().subscribe(
-      response=>{
-        this.tasks=response
-      }
-    )
+
+  refreshTasks() {
+    this.taskservice.retrieveTasks().subscribe((response: Task[]) => {
+      this.tasks = response;
+    });
   }
-  deleteTask(id:any){
-    this.taskservice.deleteTask(id).subscribe(
-      response=>{
-        this.message=`Deletion of Task ${id} successful`
-        this.refreshTasks()
-      }
-    )
+
+  deleteTask(id: any) {
+    this.taskservice.deleteTask(id).subscribe((response) => {
+      this.message = `Deletion of Task ${id} successful`;
+      this.refreshTasks();
+    });
   }
-  tasks:Task[] | undefined
-  // Tasks=[new task('To do','In progress',true),
-  // new task('assign','In progress',true),
-  // new task('evaluate','In progress',true)]
 }
