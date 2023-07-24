51a2e876212dc1530574e3ae69739632fd6da26d
PavatharaniV <vpavatharani89@gmail.com>
2023-07-24 06:37:46 +0000

    changes

--- TaskManagement/src/app/app-routing.module.ts
+++ TaskManagement/src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ import { AssignTaskComponent } from './teamLeader/assign-task/assign-task.compon
 import { authGuard } from './auth_guard/auth.guard';
 
 const routes: Routes =
-  [  
+  [   
 
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: "home", component: HomeComponent },
