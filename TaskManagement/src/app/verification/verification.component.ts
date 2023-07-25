559397398692c005938d23dbe06ef8d8cb7ffe4f
priya7667 <priyanjalibarriar091@gmail.com>
2023-07-25 18:56:37 +0000

    new one added

--- TaskManagement/src/app/verification/verification.component.ts
+++ TaskManagement/src/app/verification/verification.component.ts
@@ -1,14 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-verification',
   templateUrl: './verification.component.html',
   styleUrls: ['./verification.component.css']
 })
-export class VerificationComponent {
+export class VerifyComponent {
   verificationCode: string="";
+  verificationMessage: string = "";
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.verificationCode = params['code'];
+    });
+  }
 
-  submitVerification() {
+  submitVerification() 
+  {
+    if (this.verificationCode) {
+      const verificationUrl = 'http://localhost:8080/register'; 
+
+      this.http.post(verificationUrl, { code: this.verificationCode }).subscribe(
+        (response: any) => {
+          if (response.success) {
+            this.verificationMessage = "Account verification successful!";
+            // Perform any additional actions after successful verification if needed
+          } else {
+            this.verificationMessage = "Account verification failed. Please try again.";
+            // Handle the case where verification fails, show error message or take appropriate action
+          }
+        },
+        (error: any) => {
+          this.verificationMessage = "An error occurred while verifying your account.";
+          // Handle any error that occurs during verification
+        }
+      );
+    }
   }
 
 }
